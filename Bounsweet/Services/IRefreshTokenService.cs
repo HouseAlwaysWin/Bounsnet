c130be2b2c0724d90a5cc4992040c3604336b825
MARTIN_WANG <martin_wang@ms.dlp.com.tw>
2023-03-08 17:18:32 +0800

    add redis service

---
+++ Bounsweet/Services/IRefreshTokenService.cs
@@ -0,0 +1,14 @@
+﻿using Bounsweet.Models;
+
+namespace Bounsweet.Services
+{
+    public interface IRefreshTokenService
+    {
+        Task<bool> AddRefreshTokenAsync(RefreshToken token);
+        Task<List<RefreshToken>> GetAllRefreshTokensAsync(string id, string userName);
+        Task<RefreshToken> GetRefreshToken(string id, string userName);
+        Task RemoveRefreshTokenAsync(RefreshToken token);
+        Task RemoveRefreshTokenAsync(string id, string userName);
+        Task RevokeRefreshTokenAsync(RefreshToken token);
+    }
+}
---
+++ Bounsweet/Services/RedisService.cs
@@ -0,0 +1,43 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using static Dapper.SqlMapper;
+
+namespace Bounsweet.Services
+{
+    public class RedisService
+    {
+        private static ConcurrentDictionary<string, Lazy<ConnectionMultiplexer>> _connectionPools = new();
+
+        private string _connString;
+        private Lazy<ConnectionMultiplexer> _redisConn;
+        private ConnectionMultiplexer Instance => _redisConn.Value;
+        public IDatabase RedisClient => this.Instance.GetDatabase();
+
+        public RedisService(string connString)
+        {
+            SetRedisConfig(connString);
+        }
+
+        public void SetRedisConfig(string connString)
+        {
+            _connString = connString;
+            _redisConn = new Lazy<ConnectionMultiplexer>(() =>
+            {
+                var conn = _connectionPools.GetOrAdd(connString, new Lazy<ConnectionMultiplexer>(() => ConnectionMultiplexer.Connect(connString)));
+                return conn.Value;
+            });
+        }
+
+        public void Set<T>(string key, Dictionary<string, T> data, TimeSpan? expiry = default)
+        {
+            var newData = data.Select(d => new HashEntry(d.Key, d.Value?.ToString())).ToArray();
+            RedisClient.HashSet(key, newData);
+        }
+    }
+}
